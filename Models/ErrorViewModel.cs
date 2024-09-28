09346d2f301b1c7f6f756d3a312d044df544137a
Andrea <camposhuaman1276@gmail.com>
2024-09-28 23:36:49 +0000

    estructura Basica

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BancoPC2_CAMPOS.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
