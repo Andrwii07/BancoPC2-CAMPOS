2f6b8ccf690d0510dfa970ac1b92af753c12a1b6
Andrea <camposhuaman1276@gmail.com>
2024-09-29 05:59:21 +0000

    Correciones de lista

---
+++ ViewModel/CuentaViewModel.cs
@@ -0,0 +1,9 @@
+
+namespace BancoPC2_CAMPOS.ViewModel
+{
+    public class CuentaViewModel
+    {
+        public Cuenta? FormCuenta{get; set;}
+        public IEnumerable<Cuenta>?ListCuenta{get; set;}
+    }
+}
